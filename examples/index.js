edf7eacc4fbc511307ba69b006f102c9ce2b8d4d
kgryte <kgryte@gmail.com>
2014-11-05 01:27:09 -0800

    [UPDATE] lib, tests, examples.

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
-var module = require( './../lib' );
+var cmax = require( './../lib' );
+
+// Simulate some data...
+var data = new Array( 100 );
+
+for ( var i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.round( Math.random()*100 );
+}
+
+console.log( cmax( data ) );
+// returns [...]
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var // Expectation library:
 	chai = require( 'chai' ),
 
 	// Module to be tested:
-	lib = require( './../lib' );
+	cmax = require( './../lib' );
 
 
 // VARIABLES //
@@ -20,9 +20,41 @@ var expect = chai.expect,
 describe( 'compute-cmax', function tests() {
 
 	it( 'should export a function', function test() {
-		expect( lib ).to.be.a( 'function' );
+		expect( cmax ).to.be.a( 'function' );
 	});
 
-	it( 'should do something' );
+	it( 'should throw an error if provided a non-array', function test() {
+		var values = [
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			function(){},
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				cmax( value );
+			};
+		}
+	});
+
+	it( 'should compute the cumulative maximum', function test() {
+		var data, expected, results;
+
+		data = [ 2, 4, 5, 3, 8, 2 ];
+		expected = [ 2, 4, 5, 5, 8, 8 ];
+
+		results = cmax( data );
+
+		assert.strictEqual( results.length, expected.length );
+		assert.deepEqual( results, expected );
+	});
 
 });
